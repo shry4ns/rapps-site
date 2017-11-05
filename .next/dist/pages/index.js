4950d24433d48785f515e4bccaaec72046b33780
Achal Srinivasan <achalsrinivasan@avs-laptop.lan>
2017-11-04 21:11:56 -0500

    initial commit

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/achalsrinivasan/Dropbox/code/riceapps/rapps-site/pages/index.js?entry";
+
+var Index = function Index() {
+	return _react2.default.createElement("p", {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 2
+		}
+	}, "Hello!");
+};
+
+exports.default = Index;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTt3QkFDYixjQUFBOzthQUFBO2VBQUE7QUFBQTtBQUFBLEVBQUEsRUFEYSxBQUNiO0FBREQsQUFJQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvYWNoYWxzcmluaXZhc2FuL0Ryb3Bib3gvY29kZS9yaWNlYXBwcy9yYXBwcy1zaXRlIn0=
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Index = () => (
+	<p>Hello!</p>
+)
+
+export default Index
